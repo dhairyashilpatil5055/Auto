da45b96b57112bae2ff058b29573a196600cb88c
dhairyashilpatil5055 <100712135+dhairyashilpatil5055@users.noreply.github.com>
2022-03-02 14:58:28 +0530

    Update Selenium1.java

--- src/Selenium1.java
+++ src/Selenium1.java
@@ -4,7 +4,7 @@ public class Selenium1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hii BHAVA");
-		//Hii from Github
+		//Hello from Github
 
 	}
 
